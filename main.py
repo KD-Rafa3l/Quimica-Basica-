d7aa8436053c8a85d9fb5c6bac0dd95890fd5d15
Rafa3ldom1nguez <125380885+Rafa3ldom1nguez@users.noreply.github.com>
2024-05-08 09:32:50 -0600

    Integracion de Datos con respecto a la U
    Aquii se coloco la informacion de los datos de la Universidad

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 print(Anthony Rafael Dominguez Arriola-1639523)
+# Suponiendo que `datos_universidad` contiene la información de la universidad
+datos_universidad = {
+    "Nombre": "Univerisodad Rafael Landivar, Campus Quetzaltenango",
+}
